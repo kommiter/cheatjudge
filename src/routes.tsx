1ee83d296746a32bb7330efc022151a59d005f57
kommiter <wnsgus821@gmail.com>
2025-03-26 16:01:17 +0900

    feat: add /student ui page (#6)

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,14 @@
 import type { RouteObject } from 'react-router'
 import { Suspense, lazy } from 'react'
-
 const App = lazy(() => import('@/App.tsx'))
 const Home = lazy(() => import('@/pages/Home'))
 const Error = lazy(() => import('@/pages/Error'))
 const NotFound = lazy(() => import('@/pages/NotFound'))
+const IDE = lazy(() => import('@/pages/IDE'))
 
 export const PATH = {
   HOME: '/',
+  STUDENT: '/student',
 }
 
 const routes: RouteObject[] = [
@@ -28,6 +29,14 @@ const routes: RouteObject[] = [
       {
         path: '*',
         element: <NotFound />,
+      },
+      {
+        children: [
+          {
+            path: PATH.STUDENT,
+            element: <IDE />,
+          },
+        ],
       },
     ],
   },
